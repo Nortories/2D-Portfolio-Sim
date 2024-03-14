55bea5bc5bb5b2158d6199885f061f60e8b950cd
Josh <sooaemalelagij@gmail.com>
2024-03-13 21:27:28 -0600

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -21,41 +21,41 @@ export const dialogueData = {
 -<a href="https://github.com/Nortories/Resume/blob/main/Programming-Certificate.pdf" target="_blank">View Certificate</a>-<div></div>
 ------------------`,
 
-  nightstand: `This is Joshua's nightstand. He has a few things on it.`,
+  nightstand: `He has a few things on his desk.`,
 
-  couch: `This is Joshua's couch. He likes to relax here and watch tech shows or play video games with the family.`,
+  couch: `Joshua likes to relax here and watch tech shows or play video games with the family.`,
 
   server: `This is Joshua's server. He uses it to host his own website, plex, or store his projects. He also uses it to host his own game servers.`,
 
-  Git1: `Looks like Python is being worked on here. The program is called task-tracker. An app that includes a Graphical user interface (GUI). 
-  It logs, tracks and times what you are working on through out the day. Helping answer lifes big question of, where did all the time go? 
-  It looks like it is still being worked on but, you can check it out the progress on 
+  Git1: `Looks like Python is being worked on here. The program is called task-tracker. It is an app that includes a Graphical User Interface (GUI). 
+  It logs, tracks and times what you are working on through out the day. It helps answer lifes big question of, where did all the time go? 
+  It looks like it is still being worked on but, you can check out the progress on 
   <a href="https://github.com/Nortories/Task-Tracker" target="_blank">Github TaskTracker</a>!`,
-  Git2: `This PC is playing a Space Game. Orbital simulator made with C++, was programmed by Joshua Sooaemalelagi and Kai. 
+  Git2: `This PC is playing a Space Game orbital simulator made with C++. It was programmed by Joshua Sooaemalelagi and Kai. 
   They wrote the physics, object collision, logic, and game mechanics for this simulator. 
   Fly around in a spaceship and shoot satellites in orbit, see if you can cause a chain reaction with space junck creating more space junk. 
   You can check out the progress on 
   <a href="https://github.com/Nortories/Orbitor" target="_blank">Github Orbitor</a>!`,
 
-  tv: `That's my TV. I've been watching alot of tech videos recently, I like :
+  tv: `Joshua has been watching a lot of tech videos recently:
   <a href="https://www.youtube.com/@Fireship" target="_blank">Fireship</a>, 
   <a href="https://www.youtube.com/@NetworkChuck" target="_blank">NetworkChuck</a>,
   <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> and 
   <a href="https://www.youtube.com/@CodingWithLewis" target="_blank">Coding with Lewis</a>! too name a few.`,
 
-  desk: `This is Joshua's desk and on it is his resume. <a href="https://github.com/Nortories/Resume/blob/main/Joshua%20Resume.pdf" target="_blank">Check it out!</a>
-  There is also Contact info for him on this sticky note --Email: sooaemalelagij@gmail.com -- if you have any interesting job opportunities you should contact him!`,
+  desk: `Check out Joshua's resume on this desk. <a href="https://github.com/Nortories/Resume/blob/main/Joshua%20Resume.pdf" target="_blank">Check it out!</a>
+  His contact info is on this sticky note --Email: sooaemalelagij@gmail.com -- if you have any interesting job opportunities, contact him!`,
 
-  sideTable1: `This just a side table. Not very intresting`,
-  sideTable2: `This just a side table. Not very intresting`,
+  sideTable1: `This is just a side table. Not very intresting`,
+  sideTable2: `This is just a side table. Not very intresting`,
 
-  bookshelf: `There are a lot of program language books on this shelves. Joshua has knows many different OOP langanges including C#, C++, Python, Javascript, HTML, CSS, SQL to name a few`,
+  bookshelf: `There are a lot of programming language books on these shelves. Joshua knows many different OOP langanges including C#, C++, Python, Javascript, HTML, CSS, SQL to name a few`,
 
   exit: `If you want to exit Joshua's Sooaemalelagi portfolio, just close the tab.`,
 
-  comingSoon: `This area is coming soon. I am always learning new things and trying out differnt tech, so will need this extra space to show case all my new projects.`,
+  comingSoon: `This area is coming soon. Joshua is always learning new things and trying out differnt tech, so he will need this extra space to show case all the new projects.`,
 
-  window: `Thats a window. It's a nice day outside.`,
+  window: `It's a nice day outside.`,
 
-  easterEgg: `You found Josh! He is so focused on his work he didn't even notice you. Check out what he is working on <a href="https://github.com/Nortories/2D-Portfolio-Sim" target="_blank">Check it out</a>.`,
+  easterEgg: `You found Joshua! He is so focused on his work he didn't even notice you. Check out what he is working on <a href="https://github.com/Nortories/2D-Portfolio-Sim" target="_blank">Check it out</a>.`,
 };
