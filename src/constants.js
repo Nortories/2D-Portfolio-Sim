8d3eec7ce79b88726b63fb9d0653c503a0c606a2
Josh <sooaemalelagij@gmail.com>
2024-03-13 19:51:02 -0600

    init

---
+++ src/constants.js
@@ -0,0 +1,61 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  cer1: ` 
+  ------------------------------------------------------<div></div>
+  -----------Bachlors Degree of Science in--------------<div></div>
+  - Software Engineering with minor in Web Development -<div></div>
+  ------------- (Graduating DEC 2025) ------------------<div></div>
+  ------------------------------------------------------`,
+  cer2: `  
+---------------------------------------------<div></div>
+------------- Certificate in ----------------<div></div>
+- Computer-Aided Design and Drafting (CADD) -<div></div>
+---------- <a href="https://github.com/Nortories/Resume/blob/main/CADD-Certificate.pdf" target="_blank">View Certificate </a>------------<div></div>
+---------------------------------------------`,
+
+  cer3: `
+------------------<div></div>
+- Certificate in -<div></div>
+-- Programming --<div></div>
+-<a href="https://github.com/Nortories/Resume/blob/main/Programming-Certificate.pdf" target="_blank">View Certificate</a>-<div></div>
+------------------`,
+
+  nightstand: `This is Joshua's nightstand. He has a few things on it.`,
+
+  couch: `This is Joshua's couch. He likes to relax here and watch tech shows or play video games with the family.`,
+
+  server: `This is Joshua's server. He uses it to host his own website, plex, or store his projects. He also uses it to host his own game servers.`,
+
+  Git1: `Looks like Python is being worked on here. The program is called task-tracker. An app that includes a Graphical user interface (GUI). 
+  It logs, tracks and times what you are working on through out the day. Helping answer lifes big question of, where did all the time go? 
+  It looks like it is still being worked on but, you can check it out the progress on 
+  <a href="https://github.com/Nortories/Task-Tracker" target="_blank">Github TaskTracker</a>!`,
+  Git2: `This PC is playing a Space Game. Orbital simulator made with C++, was programmed by Joshua Sooaemalelagi and Kai. 
+  They wrote the physics, object collision, logic, and game mechanics for this simulator. 
+  Fly around in a spaceship and shoot satellites in orbit, see if you can cause a chain reaction with space junck creating more space junk. 
+  You can check out the progress on 
+  <a href="https://github.com/Nortories/Orbitor" target="_blank">Github Orbitor</a>!`,
+
+  tv: `That's my TV. I've been watching alot of tech videos recently, I like :
+  <a href="https://www.youtube.com/@Fireship" target="_blank">Fireship</a>, 
+  <a href="https://www.youtube.com/@NetworkChuck" target="_blank">NetworkChuck</a>,
+  <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> and 
+  <a href="https://www.youtube.com/@CodingWithLewis" target="_blank">Coding with Lewis</a>! too name a few.`,
+
+  desk: `This is Joshua's desk and on it is his resume. <a href="https://github.com/Nortories/Resume/blob/main/Joshua%20Resume.pdf" target="_blank">Check it out?</a>
+  There is also Contact info for him on this sticky note --Email: sooaemalelagij@gmail.com -- if you have any interesting job opportunities you should contact him!`,
+
+  sideTable1: `This just a side table. Not very intresting`,
+  sideTable2: `This just a side table. Not very intresting`,
+
+  bookshelf: `There are a lot of program language books on this shelves. Joshua has knowlage in many different OOP langanges including C#, C++, Python, Javascript, HTML, CSS, SQL to name a few`,
+
+  exit: `If you want to exit Joshua's Sooaemalelagi portfolio, just close the tab.`,
+
+  comingSoon: `This area is coming soon. I am always learning new things and trying out differnt tech, so will need this extra space to show case all my new projects.`,
+
+  window: `Thats a window. It's a nice day outside.`,
+
+  easterEgg: `You found Josh! He is so focused on his work he didn't even notice you. Check out what he is working on <a href="https://github.com/Nortories/Resume/blob/main/Joshua%20Resume.pdf" target="_blank">Check it out?</a>.`,
+};
